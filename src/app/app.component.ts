5b328ae461a49347b5820ae81f5b927e52b4256a
Magnus <kingstellander@gmail.com>
2020-06-22 10:18:33 +0200

    Add design package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  items = [
+    {
+      id: 1,
+      name: 'Apple',
+      price: 12,
+      discounted: false,
+    },
+    {
+      id: 2,
+      name: 'Orange',
+      price: 20,
+      discounted: false,
+    },
+    {
+      id: 3,
+      name: 'Banana',
+      price: 17,
+      discounted: false,
+    },
+    {
+      id: 4,
+      name: 'Grapes',
+      price: 35,
+      discounted: true,
+    },
+    {
+      id: 5,
+      name: 'Pear',
+      price: 10,
+      discounted: false,
+    },
+    {
+      id: 6,
+      name: 'Kiwi',
+      price: 6,
+      discounted: true,
+    }
+  ];
+
+
+
   title = 'tempus';
 }
