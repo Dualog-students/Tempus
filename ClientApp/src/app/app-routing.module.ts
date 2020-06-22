456daba484a3f99fc39511ef919e8f7f98515578
Sebastian Olsen <seppen.o@gmail.com>
2020-06-22 16:26:23 +0200

    Initial nav-bar commit
    Created components for the navbar and the pages linked from navbar.
    Routed the different components on the navbar.
    started styling the navbar.

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ManagerComponent } from './manager/manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'manage', component: ManagerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { DualogDesignSystemModule } from '@dualog/design-system';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ManagerComponent } from './manager/manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    DashboardComponent,
+    OverviewComponent,
+    ManagerComponent
   ],
   imports: [
     BrowserModule,
