8f801d54def7c51be360785aee10e38f096c1e11
Sebastian Olsen <seppen.o@gmail.com>
2020-06-23 12:53:52 +0200

    Removed page components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,26 +5,11 @@ import { DualogDesignSystemModule } from '@dualog/design-system';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ProfileComponent } from './profile/profile.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { OverviewComponent } from './overview/overview.component';
-import { ManagerComponent } from './manager/manager.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    ProfileComponent,
-    DashboardComponent,
-    OverviewComponent,
-    ManagerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DualogDesignSystemModule
-  ],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, DualogDesignSystemModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
