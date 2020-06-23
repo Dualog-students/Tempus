f257524a687aaa9f970145d216e87d8eaf709242
markhilb <mhilbertsen@hotmail.no>
2020-06-23 09:58:40 +0200

    Create Dto for registering user 🤠

---
+++ Tempus.API/Controllers/Dto/RegisterUserDto.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Tempus.API.Controllers.Dto
+{
+    public class RegisterUserDto
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Position { get; set; }
+    }
+}
+
