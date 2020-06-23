7ff6fed7e01c330e9433e2d603c8a25aad3c84f9
DeeaMatei <mateiandreea1999@yahoo.com>
2020-06-23 13:05:35 +0200

    Add notfound component

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component: AppComponent }, { path: '404', component: NotFoundComponent },{ path: '**', redirectTo: '/404' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
