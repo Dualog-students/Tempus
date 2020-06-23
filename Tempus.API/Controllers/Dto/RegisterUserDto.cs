8f5267d797f1ba3d8881e656eb852e46e4bebc5d
markhilb <mhilbertsen@hotmail.no>
2020-06-23 12:26:08 +0200

    Add email field to users 📮

--- Tempus.API/Controllers/Dto/RegisterUserDto.cs
+++ Tempus.API/Controllers/Dto/RegisterUserDto.cs
@@ -5,8 +5,8 @@ namespace Tempus.API.Controllers.Dto
     public class RegisterUserDto
     {
         public string Name { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         public string Position { get; set; }
     }
 }
-
