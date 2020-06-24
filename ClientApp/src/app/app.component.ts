7d80f5210d482e2aa39eb244f34ce13ea8b9ed61
David Kristoffersen <david.alta2010@gmail.com>
2020-06-24 15:52:04 +0200

    Remove navbar from login page

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -7,46 +7,12 @@ import { NavbarToggleService } from './services/navbar-toggle.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  loggedIn: boolean = false;
+
   constructor(public navbarService: NavbarToggleService) {}
 
-  items = [
-    {
-      id: 1,
-      name: 'Apple',
-      price: 12,
-      discounted: false,
-    },
-    {
-      id: 2,
-      name: 'Orange',
-      price: 20,
-      discounted: false,
-    },
-    {
-      id: 3,
-      name: 'Banana',
-      price: 17,
-      discounted: false,
-    },
-    {
-      id: 4,
-      name: 'Grapes',
-      price: 35,
-      discounted: true,
-    },
-    {
-      id: 5,
-      name: 'Pear',
-      price: 10,
-      discounted: false,
-    },
-    {
-      id: 6,
-      name: 'Kiwi',
-      price: 6,
-      discounted: true,
-    },
-  ];
-
-  title = 'tempus';
+  ngOnInit(): void {
+    // TODO: Create loggin detection component
+    //this.loggedIn = this.loggedInGuardComponent.isLoggedIn;
+  }
 }
