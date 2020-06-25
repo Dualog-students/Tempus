7e6b369caab1260db381c358178ba4caa79e9602
markhilb <mhilbertsen@hotmail.no>
2020-06-25 11:09:07 +0200

    Fix login service

--- ClientApp/src/app/services/login.service.ts
+++ ClientApp/src/app/services/login.service.ts
@@ -16,21 +16,21 @@ export class LoginService {
   ) {}
 
   async authencicateUser(email: string, password: string): Promise<boolean> {
-    return this.userProvider.login(email, password).then(
-      async (response: string) => {
-        this.sessionService.setToken(response);
-        await this.userService.fetchUser(response);
-        return true;
-      },
-      (error: any) => {
+    return this.userProvider
+      .login(email, password)
+      .then(async (response: any) => {
+        if (typeof response === 'string') {
+          this.sessionService.setToken(response);
+          await this.userService.fetchUser(response);
+          return true;
+        }
         return false;
-      },
-    );
+      });
   }
 
   async registerUser(user: any) {
-    return this.userProvider.register(user).then(async (response: any) => {
-      if (response.status === 200) {
+    return this.userProvider.register(user).then(async (response: string) => {
+      if (typeof response === 'string') {
         this.authencicateUser(user.Email, user.Password);
         return true;
       }
