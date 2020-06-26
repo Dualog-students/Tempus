e704cadc224651b7df8202ea48de2f1fadeda36d
David Kristoffersen [Dualog] <david@dualog.no>
2020-06-26 14:26:47 +0200

    Correct path and prettified

--- ClientApp/src/app/services/api/user-provider.service.ts
+++ ClientApp/src/app/services/api/user-provider.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseApiService, ApiOptions } from './base-api.service';
-import { RegisterUser } from '../../models/RegisterUser.model';
+import { RegisterUser } from '../../models/registerUser.model';
 import { User } from '../../models/user.model';
 
 @Injectable({
