8318a3b069055ad137f7cb5292714625b3246592
markhilb <mhilbertsen@hotmail.no>
2020-06-26 09:37:11 +0200

    Add path to manage-admin page in navbar 🐌

--- ClientApp/src/app/components/navbar/navbar.component.ts
+++ ClientApp/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { NavbarToggleService } from '../../services/navbar-toggle.service';
 import { LoginService } from '../../services/login.service';
-import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-navbar',
@@ -9,13 +12,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  user: User;
+
   constructor(
     public navbarService: NavbarToggleService,
     private loginService: LoginService,
     private router: Router,
+    private userService: UserService,
   ) {}
 
-  ngOnInit(): void {}
+  async ngOnInit() {
+    this.user = await this.userService.getCurrentUser();
+  }
 
   logout() {
     this.loginService.logout();
