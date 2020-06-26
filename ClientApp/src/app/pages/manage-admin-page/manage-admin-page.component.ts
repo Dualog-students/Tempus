75ce433b72257eee06f04f18a2163c39859345b4
markhilb <mhilbertsen@hotmail.no>
2020-06-26 09:16:40 +0200

    Implement toggle admin functionality ⚓

--- ClientApp/src/app/pages/manage-admin-page/manage-admin-page.component.ts
+++ ClientApp/src/app/pages/manage-admin-page/manage-admin-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
+import { UserProviderService } from '../../services/api/user-provider.service';
 
 @Component({
   selector: 'app-manage-admin-page',
@@ -9,10 +10,22 @@ import { UserService } from '../../services/user.service';
 })
 export class ManageAdminPageComponent implements OnInit {
   user: User;
+  otherUsers: User[] = [];
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private userProvider: UserProviderService,
+  ) {}
 
   async ngOnInit() {
     this.user = await this.userService.getCurrentUser();
+    const users = await this.userProvider.getAllUsers();
+    if (users) {
+      this.otherUsers = users.filter(u => u._id !== this.user._id);
+    }
   }
+
+    async toggleAdmin(user: User, status: boolean) {
+        await this.userProvider.updateUserField(user._id, "IsAdmin", status.toString());
+    }
 }
