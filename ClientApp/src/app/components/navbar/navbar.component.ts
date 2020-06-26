09bd193627117476e232654f753b70bb61ab971a
markhilb <mhilbertsen@hotmail.no>
2020-06-26 09:16:13 +0200

    Fix logout funcionality 🙀

--- ClientApp/src/app/components/navbar/navbar.component.ts
+++ ClientApp/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { NavbarToggleService } from '../../services/navbar-toggle.service';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +9,16 @@ import { NavbarToggleService } from '../../services/navbar-toggle.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor(public navbarService: NavbarToggleService, private el: ElementRef) {}
+  constructor(
+    public navbarService: NavbarToggleService,
+    private loginService: LoginService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {}
+
+  logout() {
+    this.loginService.logout();
+    this.router.navigate(['/login']);
+  }
 }
