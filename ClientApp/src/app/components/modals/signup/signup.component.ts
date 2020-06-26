2dcc24f0f9b43c8185e5d09d7631c3bdb121d78f
DeeaMatei <mateiandreea1999@yahoo.com>
2020-06-26 17:23:49 +0200

    Add Other option to Position DropDown

--- ClientApp/src/app/components/modals/signup/signup.component.ts
+++ ClientApp/src/app/components/modals/signup/signup.component.ts
@@ -36,6 +36,7 @@ export class SignupComponent implements OnInit {
     name: new FormControl('', [Validators.required]),
     partTimePercentage: new FormControl(100),
     position: new FormControl('', [Validators.required]),
+    otherPosition: new FormControl(''),
   });
 
   constructor(private loginService: LoginService, private router: Router) {}
@@ -114,5 +115,14 @@ export class SignupComponent implements OnInit {
       }
       return null;
     };
+  }
+
+  onChange(result) {
+    console.log(result);
+    if (result.position === 'Other') {
+      this.signUpForm.controls.otherPosition.setValidators(Validators.required);
+      return;
+    }
+    this.signUpForm.controls.otherPosition.clearValidators();
   }
 }
