61332c1a47d7651751e64bb10ccd578549a7ec10
markhilb <mhilbertsen@hotmail.no>
2020-06-29 10:13:39 +0200

    Implement change password 🔏

--- ClientApp/src/app/modals/edit-user-modal/edit-user-modal.component.ts
+++ ClientApp/src/app/modals/edit-user-modal/edit-user-modal.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
+import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
 import { UserProviderService } from '../../services/api/user-provider.service';
+import {
+  passwordValidator,
+  confirmPasswordValidator,
+} from '../../validators/password.validator';
+import { partTimePercentValidator } from '../../validators/part-time-percentage.validator';
 
 @Component({
   selector: 'app-edit-user-modal',
@@ -12,13 +23,17 @@ import { UserProviderService } from '../../services/api/user-provider.service';
 })
 export class EditUserModalComponent implements OnInit {
   @Input() modal: boolean;
-  @Input() field: string;
   @Output() modalChange = new EventEmitter<boolean>();
+  _field: string;
+  user: User;
 
   fgUpdateUserField = new FormGroup({
-    field: new FormControl(this.field, [Validators.required]),
-    value: new FormControl('', [Validators.required]),
-      confirmPassword: new FormControl('')
+    field: new FormControl('', [Validators.required]),
+    value: new FormControl(''),
+  });
+  fgChangePassword = new FormGroup({
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
   });
 
   constructor(
@@ -26,27 +41,70 @@ export class EditUserModalComponent implements OnInit {
     private userProvider: UserProviderService,
   ) {}
 
-    ngOnInit(): void {
-        this.fgUpdateUserField.controls.
+  async ngOnInit() {
+    this.user = await this.userService.getCurrentUser();
+    this.fgChangePassword.controls.password.setValidators([
+      Validators.required,
+      passwordValidator(this.fgChangePassword.controls.password),
+    ]);
+    this.fgChangePassword.controls.confirmPassword.setValidators([
+      Validators.required,
+      confirmPasswordValidator(
+        this.fgChangePassword.controls.confirmPassword,
+        this.fgChangePassword.controls.password,
+      ),
+    ]);
+  }
+
+  @Input()
+  set field(field: string) {
+    if (field === 'PartTimePercentage') {
+      this.fgUpdateUserField.controls.value.setValidators([
+        Validators.required,
+        partTimePercentValidator(this.fgUpdateUserField.controls.value),
+      ]);
+    } else if (field === 'Email') {
+      this.fgUpdateUserField.controls.value.setValidators([
+        Validators.email,
+        Validators.required,
+      ]);
+    } else {
+      this.fgUpdateUserField.controls.value.setValidators([
+        Validators.required,
+      ]);
     }
+    this._field = field;
+  }
 
   closeModal() {
     this.fgUpdateUserField.reset();
+    this.fgChangePassword.reset();
     this.modal = false;
     this.modalChange.emit(this.modal);
   }
 
   async saveModal() {
-    if (!this.fgUpdateUserField.valid) {
-      this.fgUpdateUserField.markAllAsTouched();
+    let fg, field, value;
+    if (this._field === 'Password') {
+      fg = this.fgChangePassword;
+      field = fg.controls.password;
+      value = fg.controls.changePassword;
+    } else {
+      fg = this.fgUpdateUserField;
+      fg.controls.field.setValue(this._field);
+      field = fg.controls.field;
+      value = fg.controls.value;
+    }
+    if (!fg.valid) {
+      fg.markAllAsTouched();
       return;
     }
-    const user = await this.userService.getCurrentUser();
     await this.userProvider.updateUserField(
-      user._id,
-      this.fgUpdateUserField.controls.field.value,
-      this.fgUpdateUserField.controls.value.value,
+      this.user._id,
+      field.value,
+      value.value,
     );
     this.closeModal();
+    location.reload();
   }
 }
