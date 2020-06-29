139502bea1b3bd562c935a51df7dabea675596b0
David Kristoffersen <david.alta2010@gmail.com>
2020-06-29 14:33:41 +0200

    Grid logger component

---
+++ ClientApp/src/app/components/grid-logger/grid-logger.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-logger',
+  templateUrl: './grid-logger.component.html',
+  styleUrls: ['./grid-logger.component.scss']
+})
+export class GridLoggerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
