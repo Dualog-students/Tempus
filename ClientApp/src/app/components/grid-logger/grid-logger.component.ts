b492a5c07e6b183c15189abaa536c53b43563ed2
David Kristoffersen <david.alta2010@gmail.com>
2020-06-30 09:53:10 +0200

    Grid logger grid design

--- ClientApp/src/app/components/grid-logger/grid-logger.component.ts
+++ ClientApp/src/app/components/grid-logger/grid-logger.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grid-logger',
   templateUrl: './grid-logger.component.html',
-  styleUrls: ['./grid-logger.component.scss']
+  styleUrls: ['./grid-logger.component.scss'],
 })
 export class GridLoggerComponent implements OnInit {
+  days = [
+    { name: 'Monday' },
+    { name: 'Tuesday' },
+    { name: 'Wednesday' },
+    { name: 'Thursday' },
+    { name: 'Friday' },
+    { name: 'Saturday' },
+    { name: 'Sunday' },
+  ];
 
-  constructor() { }
+  projects = [
+    {
+      name: 'project1',
+      hours: 53,
+    },
+    {
+      name: 'project2',
+      hours: 65,
+    },
+    {
+      name: 'project3',
+      hours: 15,
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAdd(day) {
+    this.projects.push({
+      name: 'project1',
+      hours: 53,
+    });
   }
-
 }
