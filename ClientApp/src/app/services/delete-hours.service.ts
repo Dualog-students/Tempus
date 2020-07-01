2c1d8682b8592d6d3bfcaf1a6dcfd407f1b8462c
David Kristoffersen <david.alta2010@gmail.com>
2020-07-01 15:40:11 +0200

    Delete hours service end point

--- ClientApp/src/app/services/delete-hours.service.ts
+++ ClientApp/src/app/services/delete-hours.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BaseApiService, ApiOptions } from './api/base-api.service';
-import { RegisterUser } from '../models/registerUser.model';
-import { User } from '../models/user.model';
 import { Hours } from '../models/hours.model';
 
 @Injectable({
@@ -11,7 +9,4 @@ export class DeleteHoursService extends BaseApiService {
   constructor(protected apiOptions: ApiOptions) {
     super(apiOptions);
   }
-
-  deleteHours = (id: string, hours: Hours): Promise<string> =>
-    super.post<string>(id + '/delete-hours', hours);
 }
