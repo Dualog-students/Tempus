00dabf202c90788b8ea900b2474e7eef4e121766
markhilb <mhilbertsen@hotmail.no>
2020-07-01 09:16:59 +0200

    Create manage-user page 📄

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { ManageAdminPageComponent } from './pages/manage-admin-page/manage-admin-page.component';
 import { ManagementPageComponent } from './pages/management-page/management-page.component';
+import { ManageUserPageComponent } from './pages/manage-user-page/manage-user-page.component';
 import { OverviewPageComponent } from './pages/overview-page/overview-page.component';
 import {
   IsLoggedInGuard,
@@ -39,6 +40,11 @@ const routes: Routes = [
   {
     path: 'management',
     component: ManagementPageComponent,
+    canActivate: [IsAdminGuard],
+  },
+  {
+    path: 'manage-user/:id',
+    component: ManageUserPageComponent,
     canActivate: [IsAdminGuard],
   },
   { path: '404', component: NotFoundComponent },
