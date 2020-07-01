eaffb1c480bf206830c5f0e05b0160b0a956222c
markhilb <mhilbertsen@hotmail.no>
2020-07-01 09:31:10 +0200

    Change structure of hours object ♻

--- Tempus.API/Controllers/Dto/HoursDto.cs
+++ Tempus.API/Controllers/Dto/HoursDto.cs
@@ -8,5 +8,6 @@ namespace Tempus.API.Controllers.Dto
         public Int64 Date { get; set; }
         public float Hours { get; set; }
         public string Project { get; set; }
+        public string Notes { get; set; }
     }
 }
