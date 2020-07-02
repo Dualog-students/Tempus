cb9969790668bf3886cc1e2b09714d82d1444927
DeeaMatei <mateiandreea1999@yahoo.com>
2020-07-02 12:31:26 +0200

    Revert to master User model

--- ClientApp/src/app/models/user.model.ts
+++ ClientApp/src/app/models/user.model.ts
@@ -1,11 +1,9 @@
-import { Hours } from './hours.model';
-
 export class User {
   _id: string;
   Name: string;
   Email: string;
   Position: string;
   IsAdmin?: boolean;
-  Hours: Hours[];
+  Hours: {};
   PartTimePercentage: number;
 }
