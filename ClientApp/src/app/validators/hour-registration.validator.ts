6364ec47600018c3ea89490158448228cb8e10e6
Sebastian Olsen <seppen.o@gmail.com>
2020-07-03 12:56:58 +0200

    Change infomessage for existing project on date

--- ClientApp/src/app/validators/hour-registration.validator.ts
+++ ClientApp/src/app/validators/hour-registration.validator.ts
@@ -4,7 +4,7 @@ export function validateRange(min: number, max: number): ValidatorFn {
   return (control: AbstractControl): { [key: string]: string } | null => {
     if (
       control.value !== undefined &&
-      (isNaN(control.value) || control.value < min || control.value > max)
+      (isNaN(control.value) || control.value <= min || control.value > max)
     ) {
       return { range: `Should be between ${min} and ${max}` };
     }
@@ -21,17 +21,16 @@ export function validateChange(c: AbstractControl) {
       if (this.hoursKey in existingHours[c.value.project.name]) {
         // The user has hours on this date on the project
         this.displayInfoMessage = true;
-        if (
-          c.value.hours ===
-          existingHours[c.value.project.name][this.hoursKey].Hours
-        ) {
+        const oldEntry = existingHours[c.value.project.name][this.hoursKey];
+        if (c.value.hours === oldEntry.Hours) {
           // These hours already exist for the project on this date
           this.infoMessage = 'These hours have already been registered';
           return { entryExists: true };
         } else if (!this.edit) {
           // Trying to add new hours to existing project on this date
-          this.infoMessage = `There are already registered hours on ${c.value.project.name} for ${this.hoursKey}`;
-          return { entryExists: true };
+          this.infoMessage = `Registering will overwrite ${oldEntry.Hours} hours
+          registered on ${c.value.project.name} for ${this.hoursKey}`;
+          return;
         }
       }
     }
