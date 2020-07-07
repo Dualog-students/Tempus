e426c11b046067a7b8c299f9092e5a0c2869ec7a
Andreea-Maria Matei [Dualog] <matei@dualog.no>
2020-07-07 15:09:23 +0200

    Temporary data for graph

--- ClientApp/src/app/components/graphs/graphs.component.ts
+++ ClientApp/src/app/components/graphs/graphs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
 import { Hours } from 'src/app/models/hours.model';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-graphs',
@@ -11,23 +12,22 @@ import { Hours } from 'src/app/models/hours.model';
 export class GraphsComponent implements OnInit {
   user: User;
   hours: Hours;
+  test = [];
   constructor(private userService: UserService) {}
   async ngOnInit() {
     this.user = await this.userService.getCurrentUser();
-    console.log(this.user.Hours);
+    Object.values(this.user.Hours).map((hours: any) => {
+      Object.entries(hours).map(([hours2]: any) => {
+        this.test.push(hours2);
+        console.log(this.test);
+      });
+    });
   }
-
-  dateString = '30/06/2020'.split('/');
-  test = new Date(
-    +this.dateString[2],
-    +this.dateString[1] - 1,
-    +this.dateString[0],
-  );
 
   series = [
     {
       name: 'Hours',
-      data: this.hours,
+      data: this.test,
     },
   ];
 
