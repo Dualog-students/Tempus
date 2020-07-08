0707bbb7ebc63f405fc3451dc095c1b6e518dba4
Sebastian Olsen <seppen.o@gmail.com>
2020-07-08 08:53:00 +0200

    Change HoursDto to use int for hours

--- Tempus.API/Controllers/Dto/HoursDto.cs
+++ Tempus.API/Controllers/Dto/HoursDto.cs
@@ -6,7 +6,7 @@ namespace Tempus.API.Controllers.Dto
     public class HoursDto
     {
         public Int64 Date { get; set; }
-        public float Hours { get; set; }
+        public Int16 Hours { get; set; }
         public string Project { get; set; }
         public string Notes { get; set; }
     }
